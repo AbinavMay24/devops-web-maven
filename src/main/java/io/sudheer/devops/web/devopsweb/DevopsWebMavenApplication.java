25d440cc39d29aff8df960df2180d338dc7c233e
veersudhir83 <veersudhir83@gmail.com>
2017-07-11 17:44:08 +0530

    new files using maven build for devops-web

---
+++ src/main/java/io/sudheer/devops/web/devopsweb/DevopsWebMavenApplication.java
@@ -0,0 +1,12 @@
+package io.sudheer.devops.web.devopsweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsWebMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsWebMavenApplication.class, args);
+	}
+}
---
+++ src/test/java/io/sudheer/devops/web/devopsweb/DevopsWebMavenApplicationTests.java
@@ -0,0 +1,16 @@
+package io.sudheer.devops.web.devopsweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DevopsWebMavenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
