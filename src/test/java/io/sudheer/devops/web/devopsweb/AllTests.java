9b1cb0d11c1d9985668c6a4eac5593565dcfdf1e
veersudhir83 <veersudhir83@gmail.com>
2017-07-12 09:29:44 +0530

    Converted to Maven Project

---
+++ src/test/java/io/sudheer/devops/web/devopsweb/AllTests.java
@@ -0,0 +1,14 @@
+package io.sudheer.devops.web.devopsweb;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	MyTests.class,
+    })
+
+public class AllTests {
+
+}
